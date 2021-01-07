c2dcdce5dd7d8d92b84de46520d3a8b494d18ff7
Sunny Guan <sunny.peng.guan@gmail.com>
2021-01-06 19:11:31 -0800

    finished routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import pymongo
 
 from dotenv import load_dotenv
@@ -11,19 +11,80 @@ users = db["users"]
 
 # print(users.find_one())
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
+def clean(obj):
+    if obj:
+        obj['_id'] = str(obj['_id'])
+
+# get all user and their data
+# no parameters required
 @app.route('/users', methods=['GET'])
+def getAll():
+    res = users.find({})
+    arr = []
+    for doc in res:
+        clean(doc)
+        arr.append(doc)
+    return jsonify(arr)
+
+
+# route: /get?name=NAME
+# example: /get?name=bob
+@app.route('/get', methods=['GET'])
 def respond():
     name = request.args.get('name')
     res = users.find_one({'name': name})
-    res['_id'] = str(res['_id'])
-    return jsonify(res)
+    if res:
+        clean(res)
+        return jsonify(res)
+    else:
+        return jsonify([])
 
+# route: /add?name=NAME&item=ITEM
+# example: /add?name=bob&item=watermelon
+@app.route('/add', methods=['GET'])
+def add():
+    name = {'name': request.args.get('name')}
+    item = request.args.get('item')
+    col = users.find_one({'name': name['name']})
+    print(col)
+    if col:
+        col = col['food']
+        col.append(item)
+        users.update_one(name, {'$set': {'food': col}})
+    else:
+        users.insert_one({
+            'name': name['name'],
+            'food': [item]
+        })
+    upd = users.find_one({'name': name['name']})
+    clean(upd)
+    return jsonify(upd)
+
+# route: /remove?name=NAME&item=ITEM
+# example: /remove?name=bob&item=watermelon
+@app.route('/remove', methods=['GET'])
+def remove():
+    name = {'name': request.args.get('name')}
+    item = request.args.get('item')
+    col = users.find_one({'name': name['name']})
+    if col:
+        col = col['food']
+        try:
+            col.remove(item)
+        except:
+            pass
+        users.update_one(name, {'$set': {'food': col}})
+        upd = users.find_one({'name': name['name']})
+        clean(upd)
+        return jsonify(upd)
+    else:
+        return jsonify([])
 
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return send_from_directory('docs', 'index.html')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
