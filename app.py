eaa1666280483ac1e3c78a4265a252f4add48772
Sunny Guan <sunny.peng.guan@gmail.com>
2021-01-06 17:33:19 -0800

    added dummy data

--- app.py
+++ app.py
@@ -1,51 +1,21 @@
-# app.py
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
+sample_data = {
+        "joe": ["apple", "orange"],
+        "john": ["eggs", "milk"],
+        "bob": ["rice", "pasta"]
+}
+
+@app.route('/users', methods=['GET'])
 def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+    name = request.args.get('name')
+    return jsonify(sample_data[name])
 
-    # For debugging
-    print(f"got name {name}")
 
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Hello World!</h1>"
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
